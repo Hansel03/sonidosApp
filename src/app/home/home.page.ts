8129ac73a0e63bdf109b357093ab5b0fa4ed3f09
Hansel <hanselcapillo@hotmail.com>
2020-04-14 12:06:49 -0500

    recorrer la lista de animales

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ANIMALES } from '../data/data.animales';
+import { AnimalInterface } from '../interfaces/animal.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  animales: AnimalInterface[];
 
-  constructor() {}
-
+  constructor() {
+    this.animales = Object.assign([], ANIMALES);
+  }
 }
